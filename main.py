bbec9421cd7c3b9006585c493d5f67d0622fa9b3
Daniel Krzeminski <dankrzeminski32@gmail.com>
2021-04-07 19:19:13 -0500

    Checked for similar words inside SQL database

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import mysql.connector
+from difflib import SequenceMatcher, get_close_matches
+
 
 con = mysql.connector.connect(
 user = "ardit700_student",
@@ -7,3 +9,20 @@ host="108.167.140.122",
 database="ardit700_pm1database"
 )
 
+cursor = con.cursor()
+
+word = input("Enter a word: ")
+
+query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s' " % word)
+results = cursor.fetchall()
+
+if results:
+    for result in results:
+        print(result[1])
+else:
+    query2 = cursor.execute("SELECT Expression FROM Dictionary")
+    expressions = [b[0] for b in cursor.fetchall()]
+
+    matches = get_close_matches(word, expressions)
+    print(matches)
+    #print("did you mean %s instead? (Y/N)" % )
